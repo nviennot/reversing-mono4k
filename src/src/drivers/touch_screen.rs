e994ac58de9099c262ee597951464770afdbbba7
Nicolas Viennot <nicolas@viennot.biz>
2022-01-17 03:11:26 -0500

    wip

--- src/src/drivers/touch_screen.rs
+++ src/src/drivers/touch_screen.rs
@@ -22,89 +22,90 @@ pub struct TouchScreen {
 }
 
 impl TouchScreen {
-    const TOUCH_PRESSURE_THRESHOLD: u32 = 30; // 40%
-
-    const NUM_SAMPLES_DEBOUNCE: usize = 16;
-    const SAMPLE_DELAY_US: u32 = 1_000;
+    // Set to 0 to disable the pressure detection.
+    // 30% is too little. points can jump around.
+    const TOUCH_PRESSURE_THRESHOLD: u32 = 35;
 
     pub fn has_touch(&self) -> bool {
         self.touch_active.is_low()
     }
 
-    pub fn is_touch_stable(&mut self, delay: &mut Delay) -> bool {
-        const WAIT_MILLIS: usize = 10;
-
-        if !self.has_touch() {
-            return false;
-        }
-
-        let mut missed_count = 0u8;
-
-        for _ in 0..WAIT_MILLIS {
-            delay.delay_ms(1u8);
-            if !self.has_touch() {
-                missed_count += 1;
-                if missed_count > 2 {
-                    return false;
-                }
-            }
-        }
-
-        true
-    }
-
     pub fn read_x_y(&mut self, delay: &mut Delay) -> Option<(u16, u16)> {
-        if !self.is_touch_stable(delay) {
+        if !self.has_touch() {
             return None;
         }
 
-        let (mut x_sum, mut y_sum) = (0u32, 0u32);
+        const NUM_SAMPLES: usize = 10;
 
-        for _ in 0..Self::NUM_SAMPLES_DEBOUNCE {
-            if let Some((x,y)) = self.read_x_y_once() {
-                x_sum += x as u32;
-                y_sum += y as u32;
-            } else {
-                return None;
+        let mut xs = [0u16; NUM_SAMPLES];
+        let mut ys = [0u16; NUM_SAMPLES];
+        let mut index: usize = 0;
+        let mut valid_samples: u16 = 0;
+
+        // We're looking for a continuous stream of NUM_SAMPLES valid samples.
+        // As soon as we do, we average the (x,y) values we got.
+        // Samples are taken every 1ms.
+        for _ in 0..10*NUM_SAMPLES {
+            valid_samples <<= 1;
+
+            if let Some((x,y)) = self.read_raw_x_y_once() {
+                valid_samples |= 1;
+                xs[index] = x;
+                ys[index] = y;
+                index += 1;
+                if index == NUM_SAMPLES {
+                    index = 0;
+                }
             }
+
+            if valid_samples == (1 << NUM_SAMPLES)-1  {
+                let x_sum: u32 = xs.iter().map(|v| *v as u32).sum();
+                let y_sum: u32 = ys.iter().map(|v| *v as u32).sum();
+
+                let x = (x_sum / NUM_SAMPLES as u32) as u16;
+                let y = (y_sum / NUM_SAMPLES as u32) as u16;
+
+                const MAX_X: u16 = 1 << 12;
+                let x = (MAX_X - x)/11 - 36;
+                let y = y/15 - 15;
+
+                return Some((x,y))
+            }
+
+            delay.delay_ms(1u8);
         }
 
-        let x = (x_sum / Self::NUM_SAMPLES_DEBOUNCE as u32) as u16;
-        let y = (y_sum / Self::NUM_SAMPLES_DEBOUNCE as u32) as u16;
-
-        const MAX_X: u16 = 1 << 12;
-        let x = (MAX_X - x)/11 - 36;
-        let y = y/15 - 15;
-
-        Some((x,y))
+        None
     }
 
     // Returns (x,y) coordinates if a touch is detected
-    pub fn read_x_y_once(&mut self) -> Option<(u16, u16)> {
-        // The touch wire better be on
+    fn read_raw_x_y_once(&mut self) -> Option<(u16, u16)> {
+        // The touch wire better be on.
         if !self.has_touch() {
             return None;
         }
 
         let x = self.read_cmd(0x90) >> 4; // swapping MSB -> LSB gives 0x09
         let y = self.read_cmd(0xD0) >> 4; // swapping MSB -> LSB gives 0x0B
-        let z = self.read_cmd(0x16);
 
         // Touch not detected
         if x < 10 || y < 10 {
             return None;
         }
 
-        // z/y seems to measure pressure.
-        // 40% is a reasonnable threshold.
-        if (100*z as u32) < (Self::TOUCH_PRESSURE_THRESHOLD * y as u32) {
-            return None;
+        if Self::TOUCH_PRESSURE_THRESHOLD > 0 {
+            let z = self.read_cmd(0x16);
+
+            // z/y seems to measure pressure.
+            if (100*z as u32) < (Self::TOUCH_PRESSURE_THRESHOLD * y as u32) {
+                return None;
+            }
         }
 
         Some((x,y))
     }
 
-    pub fn read_cmd(&mut self, cmd: u8) -> u16 {
+    fn read_cmd(&mut self, cmd: u8) -> u16 {
         self.cs.set_low();
 
         Self::delay(2);
@@ -122,23 +123,8 @@ impl TouchScreen {
         result
     }
 
-    pub fn write_cmd(&mut self, cmd: u8, data: u16) {
-        self.cs.set_low();
-
-        Self::delay(2);
-        self.exchange_data(cmd);
-        Self::delay(10);
-
-        self.exchange_data((data >> 8) as u8);
-        self.exchange_data(data as u8);
-
-        self.cs.set_high();
-
-        Self::delay(6);
-    }
-
     // Serial data, MSB first
-    pub fn exchange_data(&mut self, mut v: u8) -> u8 {
+    fn exchange_data(&mut self, mut v: u8) -> u8 {
         let mut out: u8 = 0;
 
         self.sck.set_low();
