d6f013ffc8e8a31050034e16232ec53efef04ae6
Nicolas Viennot <nicolas@viennot.biz>
2022-02-06 01:19:56 -0500

    Move code around

--- src/src/main.rs
+++ src/src/main.rs
@@ -24,7 +24,7 @@ use drivers::{
 use embedded_graphics::pixelcolor::Rgb565;
 
 use lvgl::core::{
-    Lvgl, TouchPad, Display, InputDevice,
+    Lvgl, TouchPad, Display, InputDevice, ObjExt
 };
 
 
@@ -33,6 +33,8 @@ pub(crate) use runtime::debug;
 extern crate alloc;
 
 use core::mem::MaybeUninit;
+
+use lvgl::core::Screen;
 
 mod runtime {
     use super::*;
@@ -78,6 +80,7 @@ mod runtime {
     dispatchers=[CAN_RX1, CAN_SCE, CAN2_RX0, CAN2_RX1]
 )]
 mod app {
+
     use super::*;
 
     #[monotonic(binds = SysTick, default = true)]
@@ -96,10 +99,10 @@ mod app {
     #[local]
     struct Local {
         lvgl: Lvgl,
-        lvgl_ticks: Ticks,
+        lvgl_ticks: lvgl::core::Ticks,
         lvgl_input_device: InputDevice::<TouchPad>,
         display: Display::<RawDisplay>,
-        move_z_ui: Screen<MoveZ>,
+        move_z_ui: Screen<ui::MoveZ> ,
     }
 
     fn lvgl_init(display: RawDisplay) -> (Lvgl, Display<RawDisplay>, InputDevice<TouchPad>) {
@@ -121,7 +124,7 @@ mod app {
     #[init]
     fn init(ctx: init::Context) -> (Shared, Local, init::Monotonics) {
         rtt_target::rtt_init_print!();
-        debug!("Booting");
+        debug!("Init...");
 
         lvgl::core::Lvgl::new();
 
@@ -139,7 +142,7 @@ mod app {
 
         let last_touch_event = None;
 
-        let mut move_z_ui = MoveZ::new(&display);
+        let mut move_z_ui = ui::MoveZ::new(&display);
         // Fill the display with something before turning it on.
         display.load_screen(&mut move_z_ui);
         lvgl.run_tasks();
@@ -222,157 +225,6 @@ mod app {
     }
 }
 
-
-use lvgl::core::Screen;
-use lvgl::core::Ticks;
-use lvgl::widgets::*;
-use lvgl::core::*;
-
-#[derive(Debug)]
-enum UserAction {
-    MoveUp,
-    MoveDown,
-    StopRequested,
-    SetSpeed(f32),
-}
-
-use drivers::stepper::prelude::*;
-
-pub struct MoveZ {
-    btn_move_up: Btn<Self>,
-    btn_move_down: Btn<Self>,
-    speed_slider: Slider<Self>,
-    speed_label: Label<Self>,
-    position_label: Label<Self>,
-
-    user_action: Option<UserAction>,
-}
-
-impl MoveZ {
-    pub fn new<D>(display: &Display<D>) -> Screen::<Self> {
-        use lvgl::widgets::*;
-        use lvgl::style::*;
-        use lvgl::core::*;
-        use lvgl::prelude::*;
-
-        let spacing = 12;
-
-        let mut screen = Screen::<Self>::new(display);
-
-        let btn_move_up = Btn::new(&mut screen).apply(|obj| {
-            Label::new(obj)
-                .set_text(&CStr::from_bytes_with_nul(b"Move Up\0").unwrap());
-            obj
-            .align_to(&screen, Align::TopMid, 0, 2*spacing)
-            .add_flag(Flag::CHECKABLE)
-            .on_event(Event::Clicked, |context| {
-                let checked = context.btn_move_up.has_state(State::CHECKED);
-                context.user_action = Some(
-                    if checked { UserAction::MoveUp }
-                    else { UserAction::StopRequested }
-                );
-            });
-        });
-
-        let btn_move_down = Btn::new(&mut screen).apply(|obj| {
-            Label::new(obj)
-                .set_text(&CStr::from_bytes_with_nul(b"Move Down\0").unwrap());
-            obj
-            .align_to(&btn_move_up, Align::OutBottomMid, 0, spacing)
-            .add_flag(Flag::CHECKABLE)
-            .on_event(Event::Clicked, |context| {
-                let checked = context.btn_move_down.has_state(State::CHECKED);
-                context.user_action = Some(
-                    if checked { UserAction::MoveDown }
-                    else { UserAction::StopRequested }
-                );
-            });
-        });
-
-        let speed_slider = Slider::new(&mut screen).apply(|obj| { obj
-            .align_to(&btn_move_down, Align::OutBottomMid, 0, 2*spacing)
-            .set_range(1500, 10_000)
-            .set_value(10_000, 0)
-            .on_event(Event::ValueChanged, |context| {
-                let value = unsafe { lvgl_sys::lv_slider_get_value(context.speed_slider.raw) };
-
-                let value = (value as f32)/10000.0;
-                let value = value*value*value;
-                let value = value * 30.0;
-
-                context.user_action = Some(UserAction::SetSpeed(value));
-            });
-        });
-
-        let speed_label = Label::new(&mut screen).apply(|obj| { obj
-            .align_to(&speed_slider, Align::OutBottomLeft, 70, spacing)
-            .set_text(&CStr::from_bytes_with_nul(b"Max Speed: 5 mm/s\0").unwrap());
-        });
-
-        let position_label = Label::new(&mut screen).apply(|obj| { obj
-            .align_to(&speed_label, Align::OutBottomLeft, 0, 0)
-            .set_text(&CStr::from_bytes_with_nul(b"Position: 0 mm\0").unwrap());
-        });
-
-        Label::new(&mut screen).apply(|obj| { obj
-            .set_text(&CStr::from_bytes_with_nul(b"Turbo Resin v0.1.1\0").unwrap())
-            .align_to(&screen, Align::BottomRight, -5, -5);
-        });
-
-        let context = Self {
-            btn_move_up,
-            btn_move_down,
-            speed_slider,
-            speed_label,
-            position_label,
-
-            user_action: None,
-        };
-
-        screen.apply(|s| {
-            s.context().replace(context);
-        })
-    }
-
-    fn update(&mut self, stepper: &mut impl rtic::Mutex<T=drivers::stepper::Stepper>) {
-        match self.user_action.take() {
-            Some(UserAction::MoveUp) => {
-                stepper.lock(|s| s.set_target_relative(40.0.mm()));
-                self.btn_move_down.add_state(State::DISABLED);
-            },
-            Some(UserAction::MoveDown) => {
-                stepper.lock(|s| s.set_target_relative((-40.0).mm()));
-                self.btn_move_up.add_state(State::DISABLED);
-            }
-            Some(UserAction::StopRequested) => {
-                self.btn_move_down.add_state(State::DISABLED);
-                self.btn_move_up.add_state(State::DISABLED);
-                stepper.lock(|s| s.controlled_stop());
-            }
-            Some(UserAction::SetSpeed(v)) => stepper.lock(|s| s.set_max_speed(Some(v.mm()))),
-            None => {}
-        }
-
-
-        let (is_idle, current_position, max_speed) = stepper.lock(|s|
-            (s.is_idle(), s.current_position, s.max_speed)
-        );
-
-        if is_idle {
-            self.btn_move_up.clear_state(State::CHECKED | State::DISABLED);
-            self.btn_move_down.clear_state(State::CHECKED | State::DISABLED);
-        }
-
-        // set_text() makes a copy of the string internally.
-        self.position_label.set_text(&CStr::from_bytes_with_nul(
-            format!("Position: {:.2} mm\0", current_position.as_mm()).as_bytes()
-        ).unwrap());
-
-        self.speed_label.set_text(&CStr::from_bytes_with_nul(
-            format!("Max speed: {:.2} mm/s\0", max_speed.as_mm()).as_bytes()
-        ).unwrap());
-    }
-}
 
     /*
     fn draw_touch_event(display: &mut Display, touch_event: Option<&TouchEvent>) {
